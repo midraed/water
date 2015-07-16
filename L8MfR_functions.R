dc3bed54c88b97b250ddd8248f0e268d5711befc
midraed <guillermo.olmedo@gmail.com>
2015-07-16 00:05:22 -0300

    more progress on hot.and.cold I have to solve how to show mean(dT). Also there are some problems when n for hot and n for cold are different, on the result list.

--- L8MfR_functions.R
+++ L8MfR_functions.R
@@ -8,7 +8,7 @@ create.aoi <- function(topleft = c(x, y), bottomright= c(x, y)){
   return(asd)
 }
 
-# Better get the images from folder...! (like albedo)
+# Better get the images from a folder...! (like albedo)
 load_L8data <-  function(landsat.band, aoi=NULL){
   require(raster)
   if(grepl("[LC[:digit:]]+LGN00_B", landsat.band, perl=TRUE) == TRUE){
@@ -209,7 +209,7 @@ momentum.roughness.length <- function(method, path=getwd(), LAI, NDVI, albedo, a
   return(Z.om)
 }
 
-aerodynamic.transport <- function(Z.om, wind, height.ws=2, Z.omw = 0.0018, z1, z2, mountainous=FALSE, surf.model){
+aerodynamic.transport <- function(Z.om, wind, height.ws=2, Z.omw = 0.0018, z1=0.1, z2=2, mountainous=FALSE, surf.model){
     u200 <- wind * log(200/Z.omw)/log(height.ws/Z.omw)
     if(mountainous==TRUE){
       u200 <- u200 * (1+0.1*(surf.model$DEM-height.ws))
@@ -218,7 +218,7 @@ aerodynamic.transport <- function(Z.om, wind, height.ws=2, Z.omw = 0.0018, z1, z
     r.ah <- log(z2/z1)/friction.velocity*0.41
 }
 
-hot.and.cold <- function(method="random", path=getwd(), ETr, Rn, G, r.ah, DEM, LAI, n=1, aoi){
+hot.and.cold <- function(method="random", n=1, path=getwd(), ETr, Rn, G, r.ah, DEM, LAI, aoi){
   bright.temp.b10 <- raster(paste(path, list.files(path = path, pattern = "_toa_band10.tif"), sep=""))
   bright.temp.b11 <- raster(paste(path, list.files(path = path, pattern = "_toa_band11.tif"), sep=""))
   if(!missing(aoi)){
@@ -227,30 +227,35 @@ hot.and.cold <- function(method="random", path=getwd(), ETr, Rn, G, r.ah, DEM, L
   }
   Ts <- mean(bright.temp.b10, bright.temp.b11)*0.1
   Ts_datum <- Ts - (DEM - 702) * 6.49 / 1000
-  P <- 101.3*((293-0.0065 * dem)/293)^5.26
+  P <- 101.3*((293-0.0065 * DEM)/293)^5.26
   air.density <- 1000 * P / (1.01*(Ts)*287)
   if(method=="random"){
     if(!max(values(Ts))>=310){
-      warning(paste("Ts max value is", round(max(values(Ts)),2)))
+      warning(paste("Ts max value is", round(max(values(Ts)),2), "and I expected Ts>=310"))
       hot <- sample(which(values(Ts)==max(values(Ts))),1)  
     } else hot <- sample(which(values(Ts>310)),n) 
     if(!max(values(LAI))>=4){
-      warning(paste("LAI max value is", round(max(values(LAI)),2)))
+      warning(paste("LAI max value is", round(max(values(LAI)),2), "and I expected LAI>=4"))
       cold <- sample(which(values(LAI)==max(values(LAI))),1)  
     } else cold <- sample(which(values(LAI>4)),n)  
-    
-    
-    cold <- sample(which(values(LAI>4)),n)
   }
-  dT_hot <- (Rn[hot] - G[hot])*r_ah[hot]/(air.density[hot]*1007)
+  dT_hot <- (Rn[hot] - G[hot])*r.ah[hot]/(air.density[hot]*1007)
   lambda <- (2.501-0.00236*(Ts-273.15))  # En el paper dice por 1e6
   LE_cold <- 1.05 * ETr * lambda[cold]
   H_cold <- Rn[cold]-G[cold]-LE_cold
-  dT_cold <- H_cold*r_ah[cold]/(air.density[cold]*1007)
+  dT_cold <- H_cold*r.ah[cold]/(air.density[cold]*1007)
   a <- (dT_hot-dT_cold)/(Ts_datum[hot]-Ts_datum[cold])
   b <- (dT_hot-a)/Ts_datum[hot]
   dT <- a+b*Ts_datum
-  return(dT)
+  # Prepare and populate result list
+  result <- list()
+  result$dT <- dT
+  result$hot.and.cold <- data.frame(pixel=integer(), Ts=double(), 
+                                    LAI=double(), factor(levels = c("hot", "cold")))
+  for(i in 1:n){result$hot.and.cold[i, ] <- c(hot, Ts[hot], LAI[hot], "hot")}
+  for(i in 1:n){result$hot.and.cold[i+n, ] <- c(cold, Ts[cold], LAI[cold], "cold")}
+  #removeTmpFiles(h=0)
+  return(result)
 }
 
 
@@ -258,11 +263,12 @@ hot.and.cold <- function(method="random", path=getwd(), ETr, Rn, G, r.ah, DEM, L
 
 
 
-## Add a function to stimate all parameters for some points
+### Add a function to estimate all parameters for some points
 
 #########################################################################
 save(create.aoi, load_L8data, checkSRTMgrids, prepareSRTMdata, solar.angles,
      sw.trasmisivity, incoming.solar.radiation, albedo,
      LAI.metric, outgoing.lw.radiation, incoming.lw.radiation,
-     soil.heat.flux1, momentum.roughness.length,
+     soil.heat.flux1, momentum.roughness.length, aerodynamic.transport,
+     hot.and.cold,
      file="L8METRICforR/MfR_functions.RData")
