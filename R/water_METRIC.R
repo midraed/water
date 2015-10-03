8e2846cfacf73a2337a13475fb20752148776569
midraed <guillermo.olmedo@gmail.com>
2015-10-03 19:47:31 -0300

    Commented out progress bar To make console clear... also it's not working really well

--- R/water_METRIC.R
+++ R/water_METRIC.R
@@ -21,13 +21,13 @@ METRIC.Rn <- function(image.DN, WeatherStation, MTL, sat, thermalband,
                       alb.coeff = "Tasumi", LAI.method = "metric2010", 
                       plain = TRUE, DEM, aoi){
   path=getwd()
-  pb <- txtProgressBar(min = 0, max = 100, style = 3)
+  #pb <- txtProgressBar(min = 0, max = 100, style = 3)
   if(plain==TRUE){
     DEM <- raster(image.DN[[1]])
     values(DEM) <- WeatherStation$location$elev
   }
   surface.model <-METRICtopo(DEM)
-  setTxtProgressBar(pb, 3)
+  #setTxtProgressBar(pb, 3)
   solar.angles.r <- solarAngles(surface.model = surface.model, 
                                 WeatherStation = WeatherStation, MTL = MTL)
   Rs.inc <- incSWradiation(surface.model = surface.model, 
@@ -44,7 +44,7 @@ METRIC.Rn <- function(image.DN, WeatherStation, MTL, sat, thermalband,
   LAI <- LAI(method = LAI.method, image = image.TOAr, L=0.1, sat = sat)
   Ts <- surfaceTemperature(LAI=LAI, sat = sat, thermalband = thermalband,
                            WeatherStation = WeatherStation)
-  setTxtProgressBar(pb, 35)
+  #setTxtProgressBar(pb, 35)
   Rl.out <- outLWradiation(LAI = LAI, Ts=Ts)
   Rl.inc <- incLWradiation(WeatherStation,DEM = surface.model$DEM, 
                            solar.angles = solar.angles.r, Ts= Ts)
@@ -128,13 +128,13 @@ METRIC.EB <- function(image.DN, WeatherStation, MTL, sat,
                       Zom.method = "short.crops", anchors.method = "CITRA-MCB",
                       ETp.coef= 1.05, Z.om.ws=0.0018, ESPA = FALSE){
   path=getwd()
-  pb <- txtProgressBar(min = 0, max = 100, style = 3)
+  #pb <- txtProgressBar(min = 0, max = 100, style = 3)
   if(plain==TRUE){
     DEM <- raster(image.DN[[1]])
     values(DEM) <- WeatherStation$location$elev
   }
   surface.model <-METRICtopo(DEM)
-  setTxtProgressBar(pb, 3)
+  #setTxtProgressBar(pb, 3)
   solar.angles.r <- solarAngles(surface.model = surface.model, 
                                 WeatherStation = WeatherStation, MTL = MTL)
   Rs.inc <- incSWradiation(surface.model = surface.model, 
@@ -147,16 +147,16 @@ METRIC.EB <- function(image.DN, WeatherStation, MTL, sat,
                      incidence.hor = solar.angles.r$incidence.hor, 
                      WeatherStation=WeatherStation, ESPA = F)
   albedo <- albedo(image.SR = image.SR,  coeff=alb.coeff, sat = sat)
-  setTxtProgressBar(pb, 6)
+  #setTxtProgressBar(pb, 6)
   LAI <- LAI(method = LAI.method, image = image.TOAr, L=0.1, sat = sat)
   Ts <- surfaceTemperature(LAI=LAI, sat = sat, thermalband = thermalband,
                            WeatherStation = WeatherStation)
-  setTxtProgressBar(pb, 35)
+  #setTxtProgressBar(pb, 35)
   Rl.out <- outLWradiation(LAI = LAI, Ts=Ts)
   Rl.inc <- incLWradiation(WeatherStation,DEM = surface.model$DEM, 
                            solar.angles = solar.angles.r, Ts= Ts)
   Rn <- netRadiation(LAI, albedo, Rs.inc, Rl.inc, Rl.out)
-  setTxtProgressBar(pb, 40)
+  #setTxtProgressBar(pb, 40)
   G <- soilHeatFlux(image = image.SR, Ts=Ts,albedo=albedo, 
                     Rn=Rn, image.SR, LAI=LAI, sat = sat)
   Z.om <- momentumRoughnessLength(LAI=LAI, mountainous = TRUE, 
@@ -166,12 +166,12 @@ METRIC.EB <- function(image.DN, WeatherStation, MTL, sat,
                               albedo = albedo, Z.om = Z.om, n = 1, 
                               anchors.method = anchors.method, sat = sat, 
                               deltaTemp = 5, verbose = FALSE)
-  setTxtProgressBar(pb, 45)
+  #setTxtProgressBar(pb, 45)
   on.meta <-  TRUE
   H <- calcH(anchors = hot.and.cold, Ts = Ts, Z.om = Z.om, 
              WeatherStation = WeatherStation, ETp.coef = ETp.coef, sat = sat, 
              Z.om.ws = Z.om.ws, DEM = DEM, Rn = Rn, G = G, verbose = FALSE)
-  setTxtProgressBar(pb, 99)
+  #setTxtProgressBar(pb, 99)
   H <-  H$H
   LE <- Rn - G - H
   EB <- stack(Rn, G, H, LE, Ts)
@@ -179,7 +179,7 @@ METRIC.EB <- function(image.DN, WeatherStation, MTL, sat,
                 stack.names = c("NetRadiation", "SoilHeat", "SensibleHeat", 
                                 "LatentHeat", "surfaceTemperature"), 
                 file = "EB", overwrite=TRUE)
-  setTxtProgressBar(pb, 100)
+  #setTxtProgressBar(pb, 100)
   return(EB)
 }
   
