77d5b9a526612329e9906dde53fe7096b3a5563e
midraed <guillermo.olmedo@gmail.com>
2017-07-19 12:18:08 -0300

    Added examples to METRIC.EB

--- R/water_METRIC.R
+++ R/water_METRIC.R
@@ -138,6 +138,33 @@ METRIC.G <- function(image.DN, WeatherStation=WeatherStation, Rn,
 #' @author Guillermo F Olmedo, \email{guillermo.olmedo@@gmail.com}
 #' @references 
 #' R. G. Allen, M. Tasumi, and R. Trezza, "Satellite-based energy balance for mapping evapotranspiration with internalized calibration (METRIC) - Model" Journal of Irrigation and Drainage Engineering, vol. 133, p. 380, 2007
+#' @examples 
+#' ### Data preparation
+#' library(water)
+#' aoi <- createAoi(topleft = c(500000, -3644000), bottomright = c(526000, -3660000))
+#' raw_data_folder <- system.file("extdata", package="water")
+#' image <- loadImage(path=raw_data_folder, aoi=aoi, sat="L8")
+#' image.SR <- loadImageSR(path=raw_data_folder, aoi=aoi)
+#' csvfile <- system.file("extdata", "INTA.csv", package="water")
+#' MTLfile <- system.file("extdata", "LC82320832016040LGN00_MTL.txt", package="water")
+#' WeatherStation <- read.WSdata(WSdata = csvfile, 
+#'                               datetime.format =  "%Y/%m/%d %H:%M", 
+#'                               columns = c("datetime", "temp",
+#'                                           "RH", "pp", "radiation", "wind"), 
+#'                               lat=-33.00513, long= -68.86469, elev=927, height= 2,
+#'                               MTL=MTLfile)
+#'                               
+#' ### LSEB with default methods and no extra parameters                               
+#' Energy.Balance <- METRIC.EB(image.DN = image, image.SR = image.SR,
+#'                             plain=TRUE, aoi=aoi, n = 5, WeatherStation = WeatherStation, 
+#'                             ETp.coef = 1.2, sat="L8", alb.coeff = "Olmedo", LST.method = "SW", 
+#'                             LAI.method = "metric2010", Z.om.ws = 0.03, MTL = MTLfile)
+#' ### LSEB with "Perrier" method for Zom and extra parameters                               
+#' Energy.Balance <- METRIC.EB(image.DN = image, image.SR = image.SR,
+#'                             plain=TRUE, aoi=aoi, n = 5, WeatherStation = WeatherStation, 
+#'                             ETp.coef = 1.2, sat="L8", alb.coeff = "Olmedo", LST.method = "SW", 
+#'                             LAI.method = "metric2010", Z.om.ws = 0.03, MTL = MTLfile,
+#'                             extraParameters = c(fLAI = 0.5, h = 1.8) ) 
 #' @export
 METRIC.EB <- function(image.DN, image.SR, WeatherStation, MTL, sat = "auto",
                       thermalband, plain=TRUE, DEM, aoi,
