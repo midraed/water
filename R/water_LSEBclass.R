660bcfd4f18c944821cd9e0bdd3f8c2d23144542
Guillermo Olmedo <guillermo.olmedo@gmail.com>
2017-11-06 23:17:29 +0100

    S3 class fix in print and plot

--- R/water_LSEBclass.R
+++ R/water_LSEBclass.R
@@ -1,26 +1,28 @@
 #' Plot method for waterLSEB S3 class
-#' @param x       waterLSEB object. 
+#' @param x       waterLSEB object.
+#' @param ...     further arguments passed to or from other methods. 
 #' @author Guillermo Federico Olmedo
 #' @export
 #' @family LSEB objects related functions
 #' @method plot waterLSEB
-plot.waterLSEB <- function(x){
+plot.waterLSEB <- function(x, ...){
   plot(x$EB[[1:4]])
 }
 
 
 #' Print method for waterLSEB S3 class
 #' @param x        waterLSEB object.
+#' @param ...      further arguments passed to or from other methods.
 #' @author Guillermo Federico Olmedo
 #' @export
 #' @family LSEB objects related functions
 #' @method print waterLSEB
-print.waterLSEB <- function(x){
-  print(x$EB)
-  print(x$WeatherStation)
-  print(x$anchors)
-  print(x$anchors@data)
-  print(x$methods)
+print.waterLSEB <- function(x, ...){
+  print(x$EB, ...)
+  print(x$WeatherStation, ...)
+  print(x$anchors, ...)
+  print(x$anchors@data, ...)
+  print(x$methods, ...)
 }
 
 
